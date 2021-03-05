cbd2900c132af2170bcfe1ab33d8e30bd7ffafa7
Sevgi <sevgiyusifova98@gmail.com>
2021-03-05 09:16:14 +0400

    removed warnings, added api.js

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useReducer } from "react";
-import axios from "axios";
-import environment from "../environment/environment";
 import CollapsibleTable from "../utils/CollapsibleTable/Table";
 import { makeStyles, Typography } from "@material-ui/core";
-import VisibleElementsReducer from '../reducers/VisibleElementsReducer';
+import VisibleElementsReducer from "../reducers/VisibleElementsReducer";
 import DataReducer from "../reducers/DataReducer";
 import SearchBar from "./SearchBar";
+import { loadData } from "../api/api";
 
 const useCharacterListStyles = makeStyles({
   content: {
@@ -14,7 +13,7 @@ const useCharacterListStyles = makeStyles({
     flexDirection: "column",
   },
   counter: {
-    marginTop: 7
+    marginTop: 7,
   },
 });
 
@@ -28,7 +27,7 @@ const createCharacter = (row) => {
     origin: row.origin.name,
     location: row.location.name,
     image: row.image,
-    created: new Date(row.created).toJSON().slice(0, 10)
+    created: new Date(row.created).toJSON().slice(0, 10),
   };
 };
 
@@ -42,76 +41,71 @@ const columns = [
   "created",
 ];
 
+const TYPE = "character";
+
 const nextPage = {
-  url: '',
-  isLast: false
+  url: "",
+  isLast: false,
 };
-
-const loadData = async (nextPageUrl = '') => {
-  const url = nextPageUrl ? nextPageUrl : `${environment.baseUrl}/character`;
-
-  try {
-    if (nextPage.isLast) throw new Error('Last page');
-    const response = await axios.get(url);
-
-    if (response.status === 200) {
-      return {
-        data: response.data.results.map((character) => createCharacter(character)),
-        nextPage: response.data.info.next
-      };
-    }
-  } catch (error) {
-    console.log(error);
-    return { data: [], nextPage: '' };
-  }
-}
 
 const CharacterList = () => {
   const classes = useCharacterListStyles();
 
   const [rows, dataDispatch] = useReducer(DataReducer, []);
 
-  const [characterList, visibleDispatch] = useReducer(VisibleElementsReducer, []);
+  const [characterList, visibleDispatch] = useReducer(
+    VisibleElementsReducer,
+    []
+  );
 
   const tableRef = React.createRef();
 
-  const isScrolledToBottom = (event) => (event.target.scrollTop + event.target.clientHeight + 0.5 >= event.target.scrollHeight);
+  const isScrolledToBottom = (event) =>
+    event.target.scrollTop + event.target.clientHeight + 0.5 >=
+    event.target.scrollHeight;
 
   const handleScrollToEnd = async (event) => {
     if (isScrolledToBottom(event)) {
-      const response = await loadData(nextPage.url);
+      if (nextPage.isLast) return;
+      const response = await loadData(createCharacter, nextPage.url, TYPE);
 
       nextPage.url = response.nextPage;
       if (!nextPage.url) nextPage.isLast = true;
-      dataDispatch({ type: 'append', payload: response.data });
-      visibleDispatch({ type: 'append', payload: response.data });
+      dataDispatch({ type: "append", payload: response.data });
+      visibleDispatch({ type: "append", payload: response.data });
     }
-  }
-
+  };
 
   useEffect(() => {
     const init = async () => {
       tableRef.current.onscroll = handleScrollToEnd;
 
-      const response = await loadData();
+      const response = await loadData(createCharacter, "", TYPE);
 
       nextPage.url = response.nextPage;
-      dataDispatch({ type: 'append', payload: response.data });
-      visibleDispatch({ type: 'append', payload: response.data });
-    }
+      dataDispatch({ type: "append", payload: response.data });
+      visibleDispatch({ type: "append", payload: response.data });
+    };
 
     init();
 
     return () => {
       nextPage.isLast = false;
-    }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className={classes.content}>
       <SearchBar columns={columns} dispatch={visibleDispatch} rows={rows} />
-      <CollapsibleTable tableRef={tableRef} columns={columns} rows={characterList}></CollapsibleTable>
-      <Typography variant="caption" className={classes.counter}>Characters in list: {characterList.length}</Typography>
+      <CollapsibleTable
+        tableRef={tableRef}
+        columns={columns}
+        rows={characterList}
+      ></CollapsibleTable>
+      <Typography variant="caption" className={classes.counter}>
+        Characters in list: {characterList.length}
+      </Typography>
     </div>
   );
 };
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useReducer } from 'react';
-import axios from "axios";
-import environment from "../environment/environment.json";
+import React, { useEffect, useReducer } from "react";
 import CollapsibleTable from "../utils/CollapsibleTable/Table";
-import { makeStyles, Typography } from '@material-ui/core';
-import VisibleElementsReducer from '../reducers/VisibleElementsReducer';
-import DataReducer from '../reducers/DataReducer';
-import SearchBar from './SearchBar';
+import { makeStyles, Typography } from "@material-ui/core";
+import VisibleElementsReducer from "../reducers/VisibleElementsReducer";
+import DataReducer from "../reducers/DataReducer";
+import SearchBar from "./SearchBar";
+import { loadData } from "../api/api";
 
 const useLocationListStyles = makeStyles({
   content: {
     margin: 25,
-    display: 'flex',
-    flexDirection: 'column'
+    display: "flex",
+    flexDirection: "column",
   },
   counter: {
-      marginTop: 7
+    marginTop: 7,
   },
 });
 
@@ -23,81 +22,77 @@ const createLocation = (row) => {
     name: row.name,
     type: row.type,
     dimension: row.dimension,
-    created: new Date(row.created).toJSON().slice(0, 10)
+    created: new Date(row.created).toJSON().slice(0, 10),
   };
 };
 
 const columns = ["name", "type", "dimension", "created"];
 
+const TYPE = "location";
+
 const nextPage = {
-  url: '',
-  isLast: false
+  url: "",
+  isLast: false,
 };
-
-const loadData = async (nextPageUrl = '') => {
-  const url = nextPageUrl ? nextPageUrl : `${environment.baseUrl}/location`;
-
-  try {
-      if (nextPage.isLast) throw new Error('Last page');
-      const response = await axios.get(url);
-
-      if (response.status === 200) {
-          return {
-              data: response.data.results.map((location) => createLocation(location)),
-              nextPage: response.data.info.next
-          };
-      }
-  } catch (error) {
-      console.log(error);
-      return { data: [], nextPage: '' };
-  }
-}
 
 const LocationList = () => {
   const classes = useLocationListStyles();
 
   const [rows, dataDispatch] = useReducer(DataReducer, []);
 
-  const [locationList, visibleDispatch] = useReducer(VisibleElementsReducer, []);
+  const [locationList, visibleDispatch] = useReducer(
+    VisibleElementsReducer,
+    []
+  );
 
   const tableRef = React.createRef();
 
-  const isScrolledToBottom = (event) => (event.target.scrollTop + event.target.clientHeight + 0.5 >= event.target.scrollHeight);
+  const isScrolledToBottom = (event) =>
+    event.target.scrollTop + event.target.clientHeight + 0.5 >=
+    event.target.scrollHeight;
 
-    const handleScrollToEnd = async (event) => {
-        if (isScrolledToBottom(event)) {
-            const response = await loadData(nextPage.url);
+  const handleScrollToEnd = async (event) => {
+    if (isScrolledToBottom(event)) {
+      if (nextPage.isLast) return;
+      const response = await loadData(createLocation, nextPage.url, TYPE);
 
-            nextPage.url = response.nextPage;
-            if (!nextPage.url) nextPage.isLast = true;
-            dataDispatch({ type: 'append', payload: response.data });
-            visibleDispatch({ type: 'append', payload: response.data });
-        }
+      nextPage.url = response.nextPage;
+      if (!nextPage.url) nextPage.isLast = true;
+      dataDispatch({ type: "append", payload: response.data });
+      visibleDispatch({ type: "append", payload: response.data });
     }
+  };
 
   useEffect(() => {
     const init = async () => {
       tableRef.current.onscroll = handleScrollToEnd;
 
-      const response = await loadData();
+      const response = await loadData(createLocation, "", TYPE);
 
       nextPage.url = response.nextPage;
-      dataDispatch({ type: 'append', payload: response.data });
-      visibleDispatch({ type: 'append', payload: response.data });
-  }
+      dataDispatch({ type: "append", payload: response.data });
+      visibleDispatch({ type: "append", payload: response.data });
+    };
 
-  init();
+    init();
 
-  return () => {
+    return () => {
       nextPage.isLast = false;
-  }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className={classes.content}>
       <SearchBar columns={columns} dispatch={visibleDispatch} rows={rows} />
-            <CollapsibleTable tableRef={tableRef} columns={columns} rows={locationList}></CollapsibleTable>
-            <Typography variant="caption" className={classes.counter}>Locations in list: {locationList.length}</Typography>
+      <CollapsibleTable
+        tableRef={tableRef}
+        columns={columns}
+        rows={locationList}
+      ></CollapsibleTable>
+      <Typography variant="caption" className={classes.counter}>
+        Locations in list: {locationList.length}
+      </Typography>
     </div>
   );
 };
